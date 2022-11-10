8b9475e85e05fd7f0a5003fe94d79ecd1b3ef9ad
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-11-10 18:52:21 +0600

    services added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -32,6 +32,7 @@ const Header = () => {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
+            <li><Link to='/allServices'>Services</Link></li>
             
          {
           user?.uid ? 
